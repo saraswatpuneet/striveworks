5039c46b7380ff488b983e1c3d69f0f0a670967f
Puneet Saraswat <61435908+saraswatpuneet@users.noreply.github.com>
2021-12-06 03:40:24 -0600

    challenge initial soluton

---
+++ challenge1/longest_fragment.go
@@ -0,0 +1,64 @@
+package main
+
+// given a string S of characters a and b, with additional empty gaps represented by '?',
+// find the longest substring of S that contains only characters a and b
+// and no other characters.
+//
+// Example:
+// S = "aa??bbb", returns 3
+// S = "a?b?aab?a", returns 2
+func longestFragment(S string) int {
+	var maxLength int
+	var currentLength int
+	var currentChar byte
+	var prevChar byte
+	isPalin := isPalindrome(S)
+	// if string is palindrome, cut it in half and check if both halves are palindromes
+	if isPalin {
+		// if length is even then cut in half
+		cutLength := 0
+		if len(S)%2 == 0 {
+			cutLength = len(S) / 2
+		} else {
+			cutLength = len(S)/2
+		}
+		S = S[:cutLength]
+	}
+	for i := 0; i < len(S); i++ {
+		currentChar = S[i]
+		if currentChar == '?' {
+			if currentLength > maxLength {
+				maxLength = currentLength
+			}
+			currentLength = 0
+		} else {
+			if currentChar == prevChar {
+				currentLength++
+			} else {
+				currentLength = 1
+			}
+		}
+		prevChar = currentChar
+	}
+	if currentLength > maxLength {
+		maxLength = currentLength
+	}
+	if isPalin {
+		maxLength++
+	}
+	return maxLength
+}
+
+func main() {
+	S := "??b??"
+	println(longestFragment(S))
+}
+
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
---
+++ longestsubstring.go
@@ -0,0 +1,33 @@
+package main
+
+// find longest substring with repeating characters
+// special case: given string is a palindrome
+func longestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	if len(s) == 1 {
+		return 1
+	}
+	if isPalindrome(s) {
+		return len(s)
+	}
+	max := 0
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if isPalindrome(s[i:j]) && j-i+1 > max {
+				max = j - i + 1
+			}
+		}
+	}
+	return max
+}
+
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
